b27ddf0f57dbd19a2dc7743af12106af3dadf015
kotarott <kg221kg@gmail.com>
2022-01-09 20:14:21 +0900

    update

--- stocks/api/v1/serializers.py
+++ stocks/api/v1/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
+from profiles.models import FavoStock
 
-# class FMPSearchStockSerializer(serializers.Serializer):
-#     symbol = serializers.CharField(max_length=20)
-#     name = serializers.CharField(max_length=100)
-#     currency = serializers.CharField(max_length=3)
-#     stockExchange = serializers.CharField(max_length=100)
-#     exchangeShortName = serializers.CharField(max_length=15)
 
+class FMPSearchSymbolSerializer(serializers.Serializer):
+    symbol = serializers.CharField(max_length=20)
+    name = serializers.CharField(max_length=100)
+    currency = serializers.CharField(max_length=3)
+    stockExchange = serializers.CharField(max_length=100)
+    exchangeShortName = serializers.CharField(max_length=15)
+    user_has_liked_symbol = serializers.SerializerMethodField()
+
+    def get_user_has_liked_symbol(self, instance):
+        request = self.context.get('request')
+        return FavoStock.objects.filter(profile=request.user.profile, symbol=instance['symbol']).exists()
--- stocks/api/v1/views.py
+++ stocks/api/v1/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from stockmate.lib import fmp_api
-
+from stocks.api.v1.serializers import FMPSearchSymbolSerializer
 
 class SearchFMPSymbolAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -12,7 +12,8 @@ class SearchFMPSymbolAPIView(APIView):
     def get(self, request,kw):
         data = fmp_api.search_symbol(kw)
         if data:
-            return Response(data, status=status.HTTP_200_OK)
+            results = FMPSearchSymbolSerializer(data, many=True, context={"request": request})
+            return Response(results.data)
         else:
             return Response(
                 {
